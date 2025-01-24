8b9a53048c381e7f46e43d9e19938c6d3c179e02
Karan <email@karanshergill.com>
2025-01-24 18:16:09 +0530

    update

--- cmd/root.go
+++ cmd/root.go
@@ -3,60 +3,98 @@ package cmd
 import (
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"os"
 
 	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v3"
 )
+
+// supported modes for wordlist generation
+var modes = map[string]bool{
+	"general":     true,
+	"directories": true,
+	"files":       true,
+	"parameters":  true,
+	"extensions":  true,
+	"subdomains":  true,
+}
 
 var mode string
 
-// supported modes for wordlist generation
-var modes = map[string]string{
-	"g": "general",
-	"d": "directories",
-	"f": "files",
-	"p": "parameters",
-	"e": "extensions",
-	"s": "subdomains",
-}
 var rootCmd = &cobra.Command{
 	Use:   "fuzzgen",
 	Short: "Generate wordlists for web fuzzing",
 	Long: `A fast and simple tool to generate wordlists for web fuzzing.
 	
 Wordlist Generation Modes:
-	- g: general
-  	- d: directorues
-  	- s: subdomains
-	- f: files
-	- p: parameters
-	- e: extensions
+	- general
+  	- directories
+  	- subdomains
+	- files
+	- parameters
+	- extensions
 	
 Usage:
-	fuzzgen -m g    # Generate a general wordlist
+	fuzzgen -m general    # Generate a general wordlist
   `,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		if _, ok := modes[mode]; !ok {
-			return errors.New("Invalid mode:" + mode)
+			return errors.New("invalid mode: " + mode + " supported modes are: general, directories, subdomains, files, parameters, extensions)")
 		}
-		switch mode {
-		case "g":
-			fmt.Println("Generating general wordlist")
-			generalWordlist()
-		case "d":
-			fmt.Println("Generating directories wordlist")
-			directoriesWordlist()
-		case "f":
-			fmt.Println("Generating files wordlist")
-		case "p":
-			fmt.Println("Generating parameters wordlist")
-		case "e":
-			fmt.Println("Generating extensions wordlist")
-		case "s":
-			fmt.Println("Generating subdomains wordlist")
+
+		// print the urls for the given mode
+		err := getSourceURLs("sources.yaml", mode)
+		if err != nil {
+			return err
 		}
+
 		return nil
 	},
+}
+
+func getSourceMap(filename string) (map[string][]string, error) {
+	fmt.Println("fetching sources...")
+
+	// read the source file
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, errors.New("error reading source file: " + err.Error())
+	}
+
+	// parse the sources.yaml file into a map to get the urls
+	var sources map[string][]string
+	err = yaml.Unmarshal(data, &sources)
+	if err != nil {
+		return nil, errors.New("error unmarshalling source file: " + err.Error())
+	}
+
+	// Debugging: Print the parsed sources map
+	fmt.Printf("Parsed sources: %+v\n", sources)
+
+	return sources, nil
+}
+
+func getSourceURLs(filename string, mode string) error {
+	sources, err := getSourceMap(filename)
+	if err != nil {
+		return err
+	}
+
+	// get the urls for the given mode
+	urls, ok := sources[mode]
+	if !ok {
+		return errors.New("no URLs found for mode: " + mode)
+	}
+
+	// print the urls
+	fmt.Printf("URLs for mode '%s':\n", mode)
+	for _, url := range urls {
+		fmt.Println(url)
+	}
+
+	return nil
+
 }
 
 func init() {
@@ -68,12 +106,4 @@ func Execute() {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-}
-
-func generalWordlist() {
-	fmt.Println("started...")
-}
-
-func directoriesWordlist() {
-	fmt.Println("started...")
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module github.com/karanshergill/fuzzgen
 go 1.23.4
 
 require (
-	github.com/spf13/cobra v1.8.1
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/spf13/cobra v1.8.1
 	github.com/spf13/pflag v1.0.5 // indirect
+	gopkg.in/yaml.v3 v3.0.1
 )
